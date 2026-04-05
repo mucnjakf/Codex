bdd401cc0d80de75d8e8bf2bdec2c9cefb2ebd7b
mucnjakf <crofilix@gmail.com>
2026-04-06 00:32:57 +0200

    Implemented update category endpoint

---
+++ src/Codex.Api/Endpoints/CategoryEndpoints/UpdateCategoryEndpoint.cs
@@ -0,0 +1,49 @@
+using Codex.Api.Extensions;
+using Codex.Application.Commands.Categories;
+using Codex.Domain.Outcomes;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codex.Api.Endpoints.CategoryEndpoints;
+
+public sealed class UpdateCategoryEndpoint : IEndpoint
+{
+    public sealed record Request(string Name);
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app
+            .MapPut("api/categories/{id:guid}", Handler)
+            .WithName("UpdateCategory")
+            .WithTags("Categories");
+    }
+
+    private static async Task<IResult> Handler(
+        [FromRoute] Guid id,
+        [FromBody] Request request,
+        [FromServices] IValidator<Request> validator,
+        [FromServices] ISender sender,
+        CancellationToken cancellationToken)
+    {
+        await validator.ValidateAndThrowAsync(request, cancellationToken);
+
+        var command = new UpdateCategoryCommand(id, request.Name);
+
+        Result result = await sender.Send(command, cancellationToken);
+
+        return result.IsSuccess
+            ? Results.NoContent()
+            : result.ToProblemDetails();
+    }
+
+    public sealed class RequestValidator : AbstractValidator<Request>
+    {
+        public RequestValidator()
+        {
+            RuleFor(request => request.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .MaximumLength(30).WithMessage("Name must be at most 30 characters");
+        }
+    }
+}
